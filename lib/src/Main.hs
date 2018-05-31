2528a5e1e535805745e0f121b5c513ecae453414
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-05-31 15:21:20 +0200

    add: better error handling, avoid partial execution

--- lib/src/Main.hs
+++ lib/src/Main.hs
@@ -25,29 +25,41 @@
 
 module Main where
 
-import           Control.Monad.Except  (MonadError, MonadIO, liftIO, runExceptT,
-                                        throwError, when)
-import           Data.Aeson            (eitherDecode, encode)
-import qualified Data.ByteString.Lazy  as B
-import           Data.Semigroup        ((<>))
-import           System.Directory.Tree (anyFailed, flattenDir,
-                                        readDirectoryWith, writeDirectoryWith)
-import           System.Environment    (getArgs)
-import           System.Posix.Files    (createSymbolicLink, fileExist,
-                                        getSymbolicLinkStatus, isSymbolicLink,
-                                        removeLink)
+import           Control.Monad.Except        (MonadError, MonadIO, liftIO,
+                                              runExceptT, throwError, void,
+                                              when)
+import           Control.Monad.Writer.Strict (MonadWriter, runWriterT, tell)
+import           Data.Aeson                  (eitherDecode, encode)
+import qualified Data.ByteString.Lazy        as B
+import           Data.Semigroup              ((<>))
+import           System.Directory.Tree       (anyFailed, flattenDir,
+                                              readDirectoryWith,
+                                              writeDirectoryWith)
+import           System.Environment          (getArgs)
+import           System.FilePath.Posix       ((</>))
+import           System.Posix.Files          (createSymbolicLink, fileExist,
+                                              getSymbolicLinkStatus,
+                                              isSymbolicLink, removeLink)
 import           Types
 
-type AppContext m = (MonadIO m, MonadError String m)
+data WriteLog = Overwrite FilePath | New FilePath
+
+instance Show WriteLog where
+  show (Overwrite f) = "overwrite: " <> f
+  show (New f)       = "new: " <> f
+
+type AppContext m = (MonadIO m, MonadWriter [WriteLog] m, MonadError String m)
 
 main :: IO ()
 main = getArgs
-       >>= runExceptT . execute
+       >>= runExceptT . runWriterT . execute
        >>= finalize
   where
-    finalize :: Either String a -> IO ()
+    finalize :: Either String (a, [WriteLog]) -> IO ()
     finalize = \case
-        Right _       -> putStrLn "Success"
+        Right (_, logs)     -> do
+          void $ traverse print logs
+          putStrLn $ show (length logs) <> " files linked"
         Left  message -> error $  "Failure: " <> message
 
     execute ::  AppContext m => [String] -> m ()
@@ -60,7 +72,7 @@ main = getArgs
     process "deliver" i o = deliver i o
     process x         _ _ = throwError $ "Unknown command: " <> x
 
--- Don't ask my why I append "/./" =)
+-- Don't ask me why I append "/./" =)
 bundle ::  AppContext m => FilePath -> FilePath -> m ()
 bundle i o = liftIO  (B.writeFile o . encode =<< readDirectoryWith pure (i <> "/./"))
 
@@ -70,20 +82,32 @@ deliver i o = (liftIO . B.readFile) i
               >>= either throwError checkSuccess
   where
     linkToDirectory ::  AppContext m => String -> WithFilePath AnchoredDirTree -> m (AnchoredDirTree ())
-    linkToDirectory d = liftIO . writeDirectoryWith writeSymbolicLink . replaceRoot d
+    linkToDirectory d t = avoidOverriding (replaceRoot d t)
+                          >>= liftIO . writeDirectoryWith writeSymbolicLink
       where
         writeSymbolicLink :: FilePath -> FilePath -> IO ()
-        writeSymbolicLink target source =
-          (checkOverride target =<< fileExist target)
-          *> createSymbolicLink source target
+        writeSymbolicLink target source = do
+          alreadyExists <- fileExist target
+          when alreadyExists (removeLink target)
+          createSymbolicLink source target
 
-        checkOverride :: FilePath -> Bool -> IO ()
-        checkOverride _      False = pure ()
-        checkOverride target True  = do
-            symbolic <- isSymbolicLink <$> getSymbolicLinkStatus target
-            if not symbolic
-              then error    ("Not symbolic file already exists: " <> target)
-              else putStrLn ("Overwriting symbolic link: " <> target) *> removeLink target
+    avoidOverriding :: AppContext m => WithFilePath AnchoredDirTree -> m (WithFilePath AnchoredDirTree)
+    avoidOverriding root@(base:/tree) = checkNode base tree *> pure root
+      where
+        checkNode :: AppContext m => FilePath -> WithFilePath DirTree -> m ()
+        checkNode b (Dir n c)           = void $ traverse (checkNode (b </> n)) c
+        checkNode b (File n _)          = checkOverride (b </> n)
+        checkNode _ (Failed n exception) = throwError $ "Error happened on file: " <> n <> " with exception: " <> show exception
+
+    checkOverride :: AppContext m => FilePath -> m ()
+    checkOverride target = do
+      alreadyExists <- liftIO $ fileExist target
+      if alreadyExists
+        then do symbolic <- liftIO $ isSymbolicLink <$> getSymbolicLinkStatus target
+                if symbolic
+                  then tell [Overwrite target]
+                  else throwError $ "Collision with true file: " <> target
+        else tell [New target]
 
     checkSuccess ::  AppContext m => AnchoredDirTree () -> m ()
     checkSuccess written
