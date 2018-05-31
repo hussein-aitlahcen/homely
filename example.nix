ebe221bd026c0e3325d0956e2ce846c4b673f7a5
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-05-31 16:55:35 +0200

    refact: ensure that we have the latest dotfiles

--- example.nix
+++ example.nix
@@ -1,9 +1,12 @@
 with import <nixpkgs> {};
 with import ./homely { inherit stdenv; inherit pkgs; inherit callPackage; };
-mkHomely {
-  dotfiles = fetchgit {
-    url = "https://github.com/hussein-aitlahcen/dotfiles";
-    rev = "d43a5ba1124d7603b4714b6007d9271efbbf160e";
-    sha256 = "190w6h4fwkv17m1rm1sf1c9m6b59dpanwnfm8yrbwaxq1f5vcjar";
-  };
-}
+let
+  latest = builtins.fromJSON (builtins.readFile ./dotfiles.json);
+in
+  mkHomely {
+    dotfiles = fetchgit {
+      url    = latest.url;
+      rev    = latest.rev;
+      sha256 = latest.sha256;
+    };
+  }
--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
-nix build -f example.nix --out-link output/definition && \
-nix build -f ./lib/shell.nix --out-link output/homely && \
-./output/homely/bin/homely deliver ./output/definition/`whoami`.json ~/
+# Replace the dotfiles_url with yours, and let's go
+
+DOTFILES_URL=https://github.com/hussein-aitlahcen/dotfiles
+DOTFILES_DEFINITION_OUTPUT=./dotfiles.json
+
+function write_dotfiles_definition {
+    nix-prefetch-git --fetch-submodules $1 > $2
+}
+
+function build_dotfiles_definition {
+    nix build -f example.nix --out-link output/definition
+}
+
+function build_homely {
+    nix build -f ./lib/shell.nix --out-link output/homely
+}
+
+function deliver_dotfiles {
+    ./output/homely/bin/homely deliver ./output/definition/`whoami`.json ~/
+}
+
+write_dotfiles_definition $DOTFILES_URL $DOTFILES_DEFINITION_OUTPUT && \
+    build_dotfiles_definition && \
+    build_homely && \
+    deliver_dotfiles
