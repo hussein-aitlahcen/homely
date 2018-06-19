8eec5ec5dec75e8edd26569662199cbdfc942c86
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-06-19 14:40:42 +0200

    fix: mess

--- reinstall.sh
+++ reinstall.sh
@@ -1,12 +1,13 @@
-cd ~/git/gihub/.spacemacs.d
-git add .
-git commit -am "fix: autocommit"
-git push origin master
-cd ~/git/github/dotfiles/.spacemacs.d
-git pull origin master
-cd ~/git/github/dotfiles
-git add .spacemacs.d
-git commit -am "fix: update spacemacs config"
-git push origin master
-cd ~/git/github/homely
-./install.sh
+cd ~/git/github/.spacemacs.d && \
+    git add . && \
+    git commit -am "fix: autocommit" && \
+    git push http master && \
+    cd ~/git/github/dotfiles/.spacemacs.d && \
+    git fetch origin master && \
+    git reset --hard origin/master && \
+    cd ~/git/github/dotfiles && \
+    git add .spacemacs.d && \
+    git commit -am "fix: update spacemacs config" && \
+    git push http master && \
+    cd ~/git/github/homely && \
+    ./install.sh
